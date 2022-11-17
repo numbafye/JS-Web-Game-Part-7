2bbc5710bdce2ef92b4be971568ec597bc506b4d
unknown <saadiqgp@gmail.com>
2022-11-17 10:10:58 -0500

    added ability to walk east. Async func in nonplayablecharacter.js

--- newPlayableCharacter.js
+++ newPlayableCharacter.js
@@ -1,28 +1,40 @@
-function newPlayableCharacter(x, y) {
-    const element = newImage('assets/green-character/static.gif')
-    element.style.zIndex = 1;
+const pc = newPlayableCharacter(100, 110);
 
-    function handleDirectionChange(direction) {
-        if (direction === null) {
-            element.src = `assets/green-character/static.gif`
-        }
-        if (direction === 'west') {
-            element.src = `assets/green-character/west.gif`
-        }
-        if (direction === 'north') {
-            element.src = `assets/green-character/north.gif`
-        }
-        if (direction === 'east') {
-            element.src = `assets/green-character/east.gif`
-        }
-        if (direction === 'south') {
-            element.src = `assets/green-character/south.gif`
-        }
-    }
-
-    move(element).withArrowKeys(x, y, handleDirectionChange)
-
-    return {
-        element: element
-    }
+function newImage(url) {
+  let image = document.createElement("img");
+  image.src = url;
+  image.style.position = "absolute";
+  document.body.append(image);
+  return image;
 }
+
+
+function newPlayableCharacter(x, y) {
+  const element = newImage("assets/green-character/static.gif");
+  element.style.zIndex = 1;
+
+  function handleDirectionChange(direction) {
+    if (direction === null) {
+      element.src = `assets/green-character/static.gif`;
+    }
+    if (direction === "west") {
+      element.src = `assets/green-character/west.gif`;
+    }
+    if (direction === "north") {
+      element.src = `assets/green-character/north.gif`;
+    }
+    if (direction === "east") {
+      element.src = `assets/green-character/east.gif`;
+    }
+    if (direction === "south") {
+      element.src = `assets/green-character/south.gif`;
+    }
+  }
+
+  move(element).withArrowKeys(x, y, handleDirectionChange);
+
+  return {
+    element: element,
+  };
+}
+
